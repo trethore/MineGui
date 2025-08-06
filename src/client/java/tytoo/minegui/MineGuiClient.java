2412e279546ac8d00acf1bf6131bd091eb496630
trethore <titou.rethore@gmail.com>
2025-08-06 16:56:12 +0200

    Init

---
+++ src/client/java/tytoo/minegui/MineGuiClient.java
@@ -0,0 +1,10 @@
+package tytoo.minegui;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineGuiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
